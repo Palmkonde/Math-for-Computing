516e03057ddcc6401723ec800f8e0800bf3c5370
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-02-12 14:13:39 +0700

    Implement graph plotting

--- jacobi.py
+++ jacobi.py
@@ -1,9 +1,11 @@
 import random
+import matplotlib.pyplot as plt
 
 from typing import Tuple, List
 
+
 def generate_matrix(
-        N: int, 
+        N: int,
         range_input: Tuple[int, int]
 ) -> Tuple[List[List[int]], List[int], List[int]]:
 
@@ -14,9 +16,10 @@ def generate_matrix(
     for i in range(N):
         A[i][i] = sum(A[i]) + 1
 
-    b = [sum(A[i][j] * x[j] for j in range(N)) for i in range(N) ]
+    b = [sum(A[i][j] * x[j] for j in range(N)) for i in range(N)]
 
     return A, b, x
+
 
 def generate_hilbert_martix(
     N: int,
@@ -24,74 +27,128 @@ def generate_hilbert_martix(
 ) -> Tuple[List[List[int]], List[int], List[int]]:
 
     A = [[1/(i + j + 1) for j in range(N)] for i in range(N)]
-    b = [sum(A[i][j] * x_input[j] for j in range(N)) for i in range(N) ]
+    b = [sum(A[i][j] * x_input[j] for j in range(N)) for i in range(N)]
 
     return A, b, x
-    
+
+
 def check_epsilon_vect(pre_x, x_i, epsilon) -> bool:
     res = sum([pre_x[i] - x_i[i] for i in range(len(pre_x))])
     res **= 2
 
     return res < epsilon
 
-def solve_jacobi(A: List[List[int]], b: List[int], x_solution) -> int:
-    x_i = [i + random.uniform(-0.5, 0.5) for i in x_solution] 
-    LHS = A.copy()   
+
+def solve_jacobi(A: List[List[int]], b: List[int], x_solution) -> Tuple[int, List[List]]:
+    x_i = [i + random.uniform(-20, 20) for i in x_solution]
+    # x_i = [random.randint(-100, 100) for i in range(len(x_solution))]
+    LHS = A.copy()
     RHS = b.copy()
+
+    save_x = []
 
     it = 0
     while it <= 100:
         tmp_xi = x_i.copy()
+        save_x.append(x_i.copy())
+
         for row in range(len(LHS)):
             tmp = LHS[row][row]
             res = 0
             for col in range(len(LHS)):
-                if row == col: continue
+                if row == col:
+                    continue
                 res += tmp_xi[col] * LHS[row][col]
             x_i[row] = (RHS[row] - res) / tmp
 
         it += 1
         print(x_i)
-        if check_epsilon_vect(tmp_xi, x_i, epsilon=1e-10):
+        if check_epsilon_vect(tmp_xi, x_i, epsilon=1e-50):
             break
-    return it
-    
-def solve_Seidal(A: List[List[int]], b: List[int], x_solution) -> int:
-    x_i = [i + random.uniform(-0.5, 0.5) for i in x_solution] 
-    LHS = A.copy()   
+    return it, save_x
+
+
+def solve_Seidal(A: List[List[int]], b: List[int], x_solution) -> Tuple[int, List[List]]:
+    x_i = [i + random.uniform(-20, 20) for i in x_solution]
+    # x_i = [random.randint(-100, 100) for i in range(len(x_solution))]
+    LHS = A.copy()
     RHS = b.copy()
+
+    save_x = []
 
     it = 0
     while it <= 100:
         tmp_xi = x_i.copy()
+        save_x.append(x_i.copy())
+
         for row in range(len(LHS)):
             tmp = LHS[row][row]
             res = 0
             for col in range(len(LHS)):
-                if row == col: continue
+                if row == col:
+                    continue
                 res += x_i[col] * LHS[row][col]
             x_i[row] = (RHS[row] - res) / tmp
 
         it += 1
         print(x_i)
-        if check_epsilon_vect(tmp_xi, x_i, epsilon=1e-10):
+        if check_epsilon_vect(tmp_xi, x_i, epsilon=1e-50):
             break
-    return it
+    return it, save_x
+
+
+def matrix_multiply(A, B):
+    m = len(A)
+    p = len(A[0])
+
+    C = []
+    for i in range(m):
+        res = 0
+        for j in range(p):
+            res += A[i][j] * B[j]
+        C.append(res)
+
+    return C
+
+
+def matrix_norm(A, B):
+    vector = []
+
+    for i in range(len(A)):
+        vector.append(A[i] - B[i])
+
+    return (sum(x ** 2 for x in vector))**0.5
+
 
 RANGE_RANDOM = (-5, 5)
 if __name__ == "__main__":
     sample_size = 5
     A, b, x = generate_matrix(sample_size, RANGE_RANDOM)
     A_hill, b_hill, x_hill = generate_hilbert_martix(sample_size, x)
+    
 
     print("Start Jacobi")
-    it1 = solve_jacobi(A, b, x)
+    it1, x_list1 = solve_jacobi(A, b, x)
+    error = [matrix_norm(b, matrix_multiply(A, x_list1[i]))
+             for i in range(len(x_list1))]
+    plt.plot([i for i in range(it1)], error, label="Jacobi method")
     print("End Jacobi\n")
-    
+
     print("Start Seidal")
-    it2 = solve_Seidal(A, b, x)
+    it2, x_list2 = solve_Seidal(A, b, x)
+    error = [matrix_norm(b, matrix_multiply(A, x_list2[i]))
+             for i in range(len(x_list2))]
+    plt.plot([i for i in range(it2)], error, label="Seidal method")
     print("End Seidel\n")
 
-    print(f"Exactly solution: {x}") 
+    print(f"Exactly solution: {x}")
     print(f"iterations from jacobi: {it1}")
     print(f"iterations from seidel: {it2}")
+    
+    plt.title("Error vs Iterator")
+    plt.legend()
+    # plt.yscale("log")
+    plt.ylabel("Error")
+    plt.xlabel("iterations")
+    plt.savefig("JacobiVsSeidel.jpg")
+    plt.show()
