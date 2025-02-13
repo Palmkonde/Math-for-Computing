4da5a5df53d71b944967bf2da3486a5878e0612e
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-02-13 15:03:29 +0700

    Implement Lagrange

--- interpolation.py
+++ interpolation.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 from typing import Tuple, List
 
 def f(x: int) -> None:
-    return np.sin(x) 
+    return np.sin(9*x) 
 
-def interpolate(
-        range_input: Tuple[int, int], 
-        m: int) -> None:
+def generate_data(range_input: Tuple[int, int], m):
     a, b = range_input[0], range_input[1]
     x_i = [np.random.uniform(a, b) for _ in range(m)]
     y_i = [f(x_subs) for x_subs in x_i]
+
+    return range_input, x_i, y_i
+
+def interpolate(range_input, x_i, y_i, m) -> None:
+    a, b = range_input[0], range_input[1]
+    x_i = x_i 
+    y_i = y_i 
 
     X = []
     for x_subs in x_i:
@@ -34,12 +39,57 @@ def interpolate(
         y_solve.append(res)
             
 
-    plt.ylim(-2, 2)
+    plt.title("f(x) = sin(9x) 20 points")
+
+    plt.ylim(-6, 6)
     plt.plot(x_range, y_origin, label="origin")
     plt.plot(x_range, y_solve, label="interpole")
+    # plt.plot(x_i, y_i, "ok", label="P(x) points")
     plt.legend()
+
+def lagrange(range_input, x_i, y_i, m):
+    a, b = range_input[0], range_input[1]
+    x_i = x_i 
+    y_i = y_i
+
+    def L(index, x):
+        numerator = 1
+        decorater = 1 
+
+        for i in range(m):
+            if i == index: continue
+            numerator *= x - x_i[i]
+
+        for i in range(m):
+            if i == index: continue
+            decorater *= x_i[index] - x_i[i]
+
+        return numerator/decorater 
+
+    def P(x: int):
+        result = 0
+        for i in range(m):
+            result += y_i[i] * L(i, x)
+        
+        return result
+    
+    x_range = np.linspace(a, b, 500)
+    y_origin = [f(x_subs) for x_subs in x_range]
+    y_solve = [P(x_subs) for x_subs in x_range]
+
+
+    plt.ylim(-6, 6)
+    # plt.plot(x_range, y_origin, label="origin")
+    plt.plot(x_range, y_solve, "--", label="Lagrange")
+    plt.plot(x_i, y_i, "ok", label="P(x) points")
+    plt.legend()
+    plt.savefig("./resultImages/Lagrange2.png")
     plt.show()
-     
+    
 
 if __name__ == "__main__":
-    interpolate((-30, 30), 100)
+    range_data = (0, 4)
+    M = 20
+    range_input, x_i, y_i = generate_data(range_data, M)
+    interpolate(range_data, x_i, y_i, M)
+    lagrange(range_data, x_i, y_i, M)
